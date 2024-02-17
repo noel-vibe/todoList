3b10e359ff573120516f2837e506ab1024378ee9
Seulgi Gwak <noel25vibe@gmail.com>
2024-02-18 02:40:20 +0900

    third commit

--- main.js
+++ main.js
@@ -6,42 +6,22 @@ let mode="all";
 let filterList =[];
 let underLine = document.getElementById("under-line");
 
-//*
-tabs.forEach((menu) => 
-menu.addEventListener("click",(e) =>indicator(e))
-);
 
-function indicator(e){
-    underLine.style.left = e.currentTarget.offsetLeft +"px";
-    underLine.style.width = e.currentTarget.offsetWidth +"px";
-    underLine.style.top = e.currentTarget.offsetTop 
-                            + e.currentTarget.offsetHeight + "px";
-}
-//* tabs (모두,진행중,끝남) 클릭시 언더라인 이동*//
-
+addButton.addEventListener("click",addTask);
 taskInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        addTask();
+        addTask(event);
     }
 }); //*Enter키로 텍스트 입력하기*//
 
-addButton.addEventListener("click",addTask);
-
-
-for(let i =1; i<tabs.length; i++){
+for(let i =0; i<tabs.length; i++){
     tabs[i].addEventListener("click",function(event){
         filter(event);
     });
 }
 
-
 function addTask(){
-    let taskContent = taskInput.value.trim();
-
-    if(taskContent === ""){
-        alert("내용을 입력해주세요.");
-        return;
-    } 
+    if(taskInput.value === "") return alert("내용을 입력해주세요.");
     //* 빈칸 입력시 경고창 띄워주기*//
 
     let task = {
@@ -66,13 +46,11 @@ function render(){
 
 
 
-
-
-    let resultHTML = "";
+    let resultHTML = '';
     for(let i=0; i<list.length; i++){
         if(list[i].isComplete == true){
             resultHTML += `<div class="task">
-            <div class = "task task-done">${list[i].taskContent}</div>
+            <div class = "task-done">${list[i].taskContent}</div>
             <div class="button-box">
                 <button onclick="toggleComplete('${list[i].id}')"><i class="fi fi-rr-refresh"></i></button>
                 <button onclick="deleteTask('${list[i].id}')"><i class="fi fi-rr-trash"></i></button>
@@ -102,8 +80,7 @@ function toggleComplete(id){
             break;
         }
     }
-    render();  /////함수 만들고 부르는거 잊지뫄!
-    console.log(taskList);
+    filter();  /////함수 만들고 부르는거 잊지뫄!
 }
 
 function  deleteTask(id){
@@ -113,13 +90,14 @@ function  deleteTask(id){
             break;
         }
     }
-    render();
+    filter();
 }
 //* delete 버튼 클릭시 클릭한 리스트(i) 1줄(1) 삭제 (splice(i,1))
 
 
 function filter(event){
-    mode = event.target.id
+    if(event){mode = event.target.id;}
+
     filterList =[];
     if(mode ==="all"){
         render()
@@ -130,7 +108,7 @@ function filter(event){
             }
         }
         render();
-        console.log("진행중",filterList)
+        console.log("진행중",filterList);
     }else if(mode === "done"){
         for(let i = 0; i <taskList.length; i++){
             if(taskList[i].isComplete === true){
@@ -141,7 +119,18 @@ function filter(event){
     }
 }
 
+//*
+tabs.forEach((menu) => 
+menu.addEventListener("click",(e) =>indicator(e))
+);
 
+function indicator(e){
+    underLine.style.left = e.currentTarget.offsetLeft +"px";
+    underLine.style.width = e.currentTarget.offsetWidth +"px";
+    underLine.style.top = e.currentTarget.offsetTop 
+                            + e.currentTarget.offsetHeight + "px";
+}
+//* tabs (모두,진행중,끝남) 클릭시 언더라인 이동*//
 
 
 function randomIDGenerate(){
